b8ab83631ed2670f41cab2ba1422d4933bcef2ac
Aigeninc <vavier@gmail.com>
2026-02-13 14:56:03 -0600

    Fix: Feedback button text rotation (was upside down)

--- src/components/FeedbackWidget.tsx
+++ src/components/FeedbackWidget.tsx
@@ -121,7 +121,7 @@ export function FeedbackWidget() {
             d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z"
           />
         </svg>
-        <span className="writing-mode-vertical-rl rotate-180">Feedback</span>
+        <span className="[writing-mode:vertical-rl]">Feedback</span>
       </button>
 
       {/* Feedback Panel */}
